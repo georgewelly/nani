93ed9c3c9426e3508b72983d82e336a54d0e442f
Felix <flxli227@gmail.com>
2023-05-18 13:13:26 +0100

    basic layout of guessing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <div className="topBar">
+          <div>
+            Guess the stuff
+          </div>
+          <div>
+            icons
+          </div>
+        </div>
+        <p>Anime Number</p>
+        <div className="pictureContainer">
+          <img 
+            src="https://www.gamespot.com/a/uploads/original/1603/16030002/4111613-mha-01.png"
+          />
+        </div >
+        <div className="screenshotNumberAndSkipContainer">
+          <div className="screenshotNumberContainer">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+          </div>
+          <div>Skip</div>
+        </div>
+        <div>
+          <form action="/action_page.php">
+            <input type="text" id="guess" name="guess"/>
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
