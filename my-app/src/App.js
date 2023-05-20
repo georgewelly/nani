88441683c632b06a46d6b82a6d6a00a321885633
Felix <flxli227@gmail.com>
2023-05-20 19:54:14 +0100

    Implemented different pages - Previous Puzzle page is a placeholder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import GuessPage from './GuessPage';
+import PreviousPuzzlePage from './PreviousPuzzlePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="App">
-      <GuessPage/>
+    <div className="App">    
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<GuessPage/>} />
+          <Route path="/previous" element={<PreviousPuzzlePage />} />
+          {/* Todo Write an error page */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-app/src/PreviousPuzzlePage.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function PreviousPuzzlePage() {
+  
+  return (
+    <div className="mainContainer">
+        HIHIHI
+    </div>
+  );
+}
+
+export default PreviousPuzzlePage;
