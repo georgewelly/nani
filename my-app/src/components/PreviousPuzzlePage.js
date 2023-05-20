fa70ce1f2f200afec59f42a445408dffa5e7ba35
Felix <flxli227@gmail.com>
2023-05-20 20:13:08 +0100

    Implement rough content on Previous Puzzle Page

--- my-app/src/components/PreviousPuzzlePage.js
+++ my-app/src/components/PreviousPuzzlePage.js
@@ -2,7 +2,13 @@ function PreviousPuzzlePage() {
   
   return (
     <>
-        HIHIHI
+        <button>Play today's puzzle</button>
+        <div className="dateGrid">
+          <p className="date">18/05/2023</p>
+          <button className="playButton">Play</button>
+          <p className="date">19/05/2023</p>
+          <button className="playButton">Play</button>
+        </div>
     </>
   );
 }
