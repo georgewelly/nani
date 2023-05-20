029f4b49bd5758efbcd9c8007bbf342dbf2b190d
Felix <flxli227@gmail.com>
2023-05-20 23:28:24 +0100

    Update fake data

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -1,6 +1,5 @@
 export const puzzle = [
     {
-        date: "19/05/2023",
         answer: "hi",
         images: [
             "https://www.gamespot.com/a/uploads/original/1603/16030002/4111613-mha-01.png",
@@ -12,27 +11,25 @@ export const puzzle = [
         ]
     },
     {
-        date: "20/05/2023",
-        answer: "hi2",
+        answer: "Cat",
         images: [
-            "https://www.gamespot.com/a/uploads/original/1603/16030002/4111613-mha-01.png",
-            "https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg",
-            "https://cdn.animenewsnetwork.com/thumbnails/crop900x350gG9/cms/news.6/198178/977481.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWV-5BONoDY0aqXr0qBWjnNvk4DIbDAH62Ow&usqp=CAU",
-            "https://nationaltoday.com/wp-content/uploads/2021/12/Anime-Day-1200x834.jpg",
-            "https://ichef.bbci.co.uk/news/976/cpsprodpb/F382/production/_123883326_852a3a31-69d7-4849-81c7-8087bf630251.jpg"
+            "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_2x3.jpg",
+            "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+            "https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg",
+            "https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg",
+            "https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg",
+            "https://icatcare.org/app/uploads/2018/06/Layer-1704-1200x630.jpg"
         ]
     },
     {
-        date: "21/05/2023",
-        answer: "hi2",
+        answer: "British Kabaddi League",
         images: [
-            "https://www.gamespot.com/a/uploads/original/1603/16030002/4111613-mha-01.png",
-            "https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg",
-            "https://cdn.animenewsnetwork.com/thumbnails/crop900x350gG9/cms/news.6/198178/977481.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWV-5BONoDY0aqXr0qBWjnNvk4DIbDAH62Ow&usqp=CAU",
-            "https://nationaltoday.com/wp-content/uploads/2021/12/Anime-Day-1200x834.jpg",
-            "https://ichef.bbci.co.uk/news/976/cpsprodpb/F382/production/_123883326_852a3a31-69d7-4849-81c7-8087bf630251.jpg"
+            "https://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/DF82/production/_124381275_kabaddi_bulls.jpg",
+            "https://www.millersoils.co.uk/wp-content/uploads/2022/03/Kishan-Image-final.jpg",
+            "https://pbs.twimg.com/media/FMdDmDAXoBATjxG?format=jpg&name=large",
+            "https://i.ytimg.com/vi/HhHpvBBS0BQ/maxresdefault.jpg",
+            "https://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/3CAE/production/_124243551_ankur_desai_rakesh_kumar.jpg",
+            "https://static.wixstatic.com/media/8ce999_a25079c3a0cf4f9bb0467cc5dc2a67dc~mv2.jpg/v1/fill/w_640,h_402,al_t,q_80,usm_0.66_1.00_0.01,enc_auto/8ce999_a25079c3a0cf4f9bb0467cc5dc2a67dc~mv2.jpg"
         ]
     },
 ];
