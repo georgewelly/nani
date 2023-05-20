023262c35a6e34de266764bd1a7ea62c08663a84
Felix <flxli227@gmail.com>
2023-05-20 21:55:09 +0100

    Used the data in the file for the guesspage.

--- my-app/src/components/GuessPage.js
+++ my-app/src/components/GuessPage.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import {puzzle} from '../data'
 
 function GuessPage() {
   // Current image shown
@@ -13,16 +14,9 @@ function GuessPage() {
   // _______________________________________________________________________
 
   // Fake data section
-  let correctAnswer = "hi";
+  let correctAnswer = puzzle[0].answer;
 
-  let images = [
-    "https://www.gamespot.com/a/uploads/original/1603/16030002/4111613-mha-01.png",
-    "https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg",
-    "https://cdn.animenewsnetwork.com/thumbnails/crop900x350gG9/cms/news.6/198178/977481.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWV-5BONoDY0aqXr0qBWjnNvk4DIbDAH62Ow&usqp=CAU",
-    "https://nationaltoday.com/wp-content/uploads/2021/12/Anime-Day-1200x834.jpg",
-    "https://ichef.bbci.co.uk/news/976/cpsprodpb/F382/production/_123883326_852a3a31-69d7-4849-81c7-8087bf630251.jpg"
-  ];
+  let images = puzzle[0].images;
 
   // _______________________________________________________________________
   // Functions
