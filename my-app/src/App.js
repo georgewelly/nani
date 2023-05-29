1ec150d186a16b2b6b8cf9a502d11e22059d9980
Felix <flxli227@gmail.com>
2023-05-29 21:45:56 +0100

    Remove Ads using Javascript instead of CSS to avoid breaking Terms and Conditions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,37 @@ import PreviousPuzzlePage from './components/PreviousPuzzlePage';
 import VerticalAd1 from './components/ads/VerticalAd1';
 import VerticalAd2 from './components/ads/VerticalAd2';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 function App() {
   // Show google ads if screen bigger than a certain amount
+  const [verticalAd1IsShown, setVerticalAd1IsShown] = useState(window.innerWidth < 640 ? false : true);
+  const [verticalAd2IsShown, setverticalAd2IsShown] = useState(window.innerWidth < 800 ? false : true);
+
+  useEffect(() => {
+    // Code here will run after *every* render
+    const handleResize = ()=>{
+      if(window.innerWidth < 640){
+        setVerticalAd1IsShown(false);
+        setverticalAd2IsShown(false);     
+      }else if(window.innerWidth < 800){
+        setVerticalAd1IsShown(true);
+        setverticalAd2IsShown(false);
+      }else{
+        setVerticalAd1IsShown(true);
+        setverticalAd2IsShown(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return()=>{
+      window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+  
 
   return (
     <div className="App">
-      <VerticalAd1/>
+      {verticalAd1IsShown ? <VerticalAd1/> : ""}
       <div className="mainContainer">
         <TopBar/>
         <BrowserRouter>
@@ -22,7 +46,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </div>
-      <VerticalAd2/>
+      {verticalAd2IsShown ? <VerticalAd2/> : ""}
     </div>
   );
 }
