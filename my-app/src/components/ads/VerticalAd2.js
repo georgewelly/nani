c07bde072caa3f9442c602c377fe597194039413
Felix <flxli227@gmail.com>
2023-05-29 21:46:55 +0100

    Change class to className to solve an warning

--- my-app/src/components/ads/VerticalAd2.js
+++ my-app/src/components/ads/VerticalAd2.js
@@ -4,7 +4,7 @@ function VerticalAd2(){
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5524499148214220"
                 crossorigin="anonymous"></script>
             {/* <!-- Vertical Ad 2 --> */}
-            <ins class="adsbygoogle verticalAd2"
+            <ins className="adsbygoogle verticalAd2"
                 data-ad-client="ca-pub-5524499148214220"
                 data-ad-slot="3790220860"
                 data-ad-format="auto"
